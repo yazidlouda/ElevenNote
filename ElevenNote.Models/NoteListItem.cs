d62bfa1d494f809f6a97955e21caafe5c53975ae
yazidLouda <yalou1231@hotmail.com>
2020-11-11 14:18:55 -0500

    third commit

---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.WebAPI.Models
+{
+    public class NoteListItem
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
